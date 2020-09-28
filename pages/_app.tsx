ba927310de184a918b1b090e8d6754334e66cc53
Moonlarya <gum225@gmail.com>
2020-09-29 01:33:27 +0300

    [add] styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { FC } from "react";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+interface IMyApp {
+  Component: any;
+  pageProps: any;
 }
+
+const MyApp: FC<IMyApp> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap');
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+
 const Home = () => {
   return (
     <>
+      <GlobalStyles />
       <h1>Latest Posts</h1>
     </>
   );
